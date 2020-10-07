e755bd6607e00f4e12f13931c56a0bd70a6102ed
Pro_Dev <developerteam888@outlook.com>
2020-10-08 01:57:44 +0900

    finish

---
+++ README.md
@@ -0,0 +1,2 @@
+# uploadFiletoAzureStorage
+This is sample for uploading image file of Azure Storage, but you can customize to suit the purpose of use.
---
+++ app/src/main/java/com/microsoft/photouploader/ImageManager.java
@@ -0,0 +1,85 @@
+package com.microsoft.photouploader;
+
+import android.util.Log;
+
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import com.microsoft.azure.storage.blob.ListBlobItem;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.SecureRandom;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ImageManager {
+    public static final String storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=xxxx;AccountKey=xxxx;";
+
+    private static CloudBlobContainer getContainer() throws Exception {
+        // Retrieve storage account from connection-string.
+        CloudStorageAccount storageAccount = CloudStorageAccount
+                .parse(storageConnectionString);
+        // Create the blob client.
+        CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+
+        // Get a reference to a container.
+        // The container name must be lower case
+        CloudBlobContainer container = blobClient.getContainerReference("tel-records");
+
+        return container;
+    }
+
+    public static String UploadImage(InputStream image, int imageLength) throws Exception {
+        CloudBlobContainer container = getContainer();
+
+        container.createIfNotExists();
+
+        String imageName = randomString(10);
+
+        CloudBlockBlob imageBlob = container.getBlockBlobReference(imageName);
+        imageBlob.upload(image, imageLength);
+
+        return imageName;
+
+    }
+
+    public static String[] ListImages() throws Exception{
+        CloudBlobContainer container = getContainer();
+
+        Iterable<ListBlobItem> blobs = container.listBlobs();
+
+        LinkedList<String> blobNames = new LinkedList<>();
+        for(ListBlobItem blob: blobs) {
+            blobNames.add(((CloudBlockBlob) blob).getName());
+        }
+
+        return blobNames.toArray(new String[blobNames.size()]);
+    }
+
+    public static void GetImage(String name, OutputStream imageStream, long imageLength) throws Exception {
+        CloudBlobContainer container = getContainer();
+
+        CloudBlockBlob blob = container.getBlockBlobReference(name);
+
+        if(blob.exists()){
+            blob.downloadAttributes();
+
+            imageLength = blob.getProperties().getLength();
+
+            blob.download(imageStream);
+        }
+    }
+
+    static final String validChars = "abcdefghijklmnopqrstuvwxyz";
+    static SecureRandom rnd = new SecureRandom();
+
+    static String randomString( int len ){
+        StringBuilder sb = new StringBuilder( len );
+        for( int i = 0; i < len; i++ )
+            sb.append( validChars.charAt( rnd.nextInt(validChars.length()) ) );
+        return sb.toString();
+    }
+
+}
